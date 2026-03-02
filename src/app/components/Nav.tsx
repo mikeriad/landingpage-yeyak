cf18fa0d6c41fe9b405b8035d11d66eaa7741141
MIKE <mike@MIKEui-MacBookPro.local>
2026-03-02 18:39:17 +0900

    logo 3x + transparent bg

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -38,7 +38,7 @@ export function Nav() {
       >
         {/* Logo */}
         <div style={{ display: "flex", alignItems: "center" }}>
-          <img src={colorLogo} alt="Yeyak" style={{ height: 36, display: "block" }} />
+          <img src={colorLogo} alt="Yeyak" style={{ height: 108, display: "block", mixBlendMode: "multiply" }} />
         </div>
 
         {/* Links */}
